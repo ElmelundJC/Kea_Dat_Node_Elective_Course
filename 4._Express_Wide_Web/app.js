7bf8eacdcce995c5f32e155ad9f6229abf11cf7f
ElmelundJC <Christianelmelund@hotmail.com>
2021-03-04 11:56:06 +0100

    Express_Wide_Web project created

---
+++ 4._Express_Wide_Web/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/welcome.html')
+});
+
+// create a new page
+app.get('/dragons', (req, res) => {
+    res.sendFile(__dirname + '/public/dragons.html');
+});
+
+
+app.listen(8080, error => {
+    if (error) {
+        console.log(error);
+    }
+    console.log("Server is running on port:", 8080);
+})
