6d5265716c9c861928aaf0166b66ead991142fd2
ElmelundJC <Christianelmelund@hotmail.com>
2021-03-04 12:35:01 +0100

    change in structure in hand-in folder updated

---
+++ hand_in_assignments/1._Intro_Exercises/3.Objects.js
@@ -0,0 +1,54 @@
+// --------------------------------------
+// Objects
+// --------------------------------------
+// Exercise 1 - Retrieve value from object by key
+
+const myObj = { "message": "Hello, earthling! I bring peace." };
+
+// Log the message 
+
+console.log(myObj.message) // dot notation <-- prefered
+
+console.log(myObj["message"]) // square notation 
+
+// --------------------------------------
+// Exercise 2 - Defining an object. 
+
+// Create an object that has your name and age. 
+const person = {
+    name: "Christian",
+    age: 28
+};
+console.log(person)
+
+// --------------------------------------
+// Exercise 3 - Add a property 
+
+const stackOverflow = {};
+
+// make a rule called isAllowed and let the value be true
+
+stackOverflow.isAllowed = true;
+console.log(stackOverflow)
+
+// --------------------------------------
+// Exercise 4 - Remove a property 
+
+const thisSong = { "description": "The best song in the world." }
+
+// remove the property "description" and add a property called "about" that should say "Just a tribute." 
+
+// console.log(thisSong)
+//delete thisSong['description']
+//thisSong['about'] = "Just a tribute --> Ill' Get You Tenacious D!"
+
+delete thisSong.description;
+thisSong.about = "Just a tribute";
+
+console.log(thisSong)
+
+
+
+// --------------------------------------
+
+
