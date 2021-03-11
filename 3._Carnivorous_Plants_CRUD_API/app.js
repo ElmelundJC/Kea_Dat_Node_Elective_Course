40bd2901f1d64654f9e0c9bf30c4bb802bb27dc9
ElmelundJC <Christianelmelund@hotmail.com>
2021-03-11 11:47:47 +0100

    6th session - Carnivorous & Express project

--- 3._Carnivorous_Plants_CRUD_API/app.js
+++ 3._Carnivorous_Plants_CRUD_API/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 
-//let plants = require('./plants.json')
+app.use(express.json());
 
+// carnivorous plants
 let plants = [
     {
         id: 1,
@@ -15,9 +16,10 @@ let plants = [
 
 ];
 
+let id = plants.length;
 
 app.get('/plants', (req, res) => {
-    res.send({ plants: plants });
+    res.send({ data: plants });
 });
 
 // todo implement the other GET method
@@ -26,7 +28,42 @@ app.get('/plants/:id', (req, res) => {
     const plantId = Number(req.params.id);
     const foundPlant = plants.find(plant => plant.id === plantId);
     if (!plantId) res.status(404).send('The plant with the given ID was not found')// return a status code 404 -- not found
-    res.send({ foundPlant: foundPlant });
+    res.send({ data: foundPlant });
+});
+
+
+// bonus: use cross-env in this project and allow the user to define a custom port
+app.post("/plants/", (req, res) => {
+    const newPlant = req.body;
+    newPlant.id = ++id;
+    plants.push(newPlant);
+    res.send({ data: newPlant });
+});
+
+// finish it so that we can tell the client wther or not it found a plant to update
+app.patch("/plants/:id", (req, res) => {
+    // map
+    plants = plants.map(plant => {
+        let plantUpdated = false;
+        if (plant.id === Number(req.params.id)) {
+            plantUpdated = true;
+            // can we unpack body?? spread operater
+            return { ...plant, ...req.body, id: req.params.id }
+        }
+        return plant;
+    });
+    res.send({ data: plantUpdated });
+});
+
+app.delete("/plants/:id", (req, res) => {
+    //  accessing the array once    
+    const id = Number(req.params.id);
+    const plants = plants.filter(plant => plant.id !== id);
+
+    //   accessing the array twice
+    //   const index = plants.indexOf(plantId);
+    //   plants.splice(index, 1);
+    res.send({});
 });
 
 
--- 4._Express_Wide_Web/app.js
+++ 4._Express_Wide_Web/app.js
@@ -7,15 +7,26 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/welcome.html')
 });
 
+// PORT=3000 nodemon app.js
+// 1. For Windows people figure out how to define environment variables
+// 2. Console log the defined port
+
+
 // create a new page
 app.get('/dragons', (req, res) => {
     res.sendFile(__dirname + '/public/dragons.html');
 });
 
+// process is working as our "main", env is the environment.
+// console.log(process.env.PORT);
+// use the environment variable if it's set otherwise use a fallback port
 
-app.listen(8080, error => {
+const PORT = process.env.PORT || 8080;
+
+
+app.listen(PORT, error => {
     if (error) {
         console.log(error);
     }
-    console.log("Server is running on port:", 8080);
+    console.log("Server is running on port:", Number(PORT));
 })
