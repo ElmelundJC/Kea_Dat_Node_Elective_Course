821dcfbf6fa467e8a26f22517d89a328f731438a
ElmelundJC <Christianelmelund@hotmail.com>
2021-04-08 12:27:50 +0200

    Comments added to the hand-in assignment Fetch google page

--- hand_in_assignments/5._fetch_Google/app.js
+++ hand_in_assignments/5._fetch_Google/app.js
@@ -2,14 +2,16 @@ const express = require("express");
 const app = express();
 
 const fetch = require("node-fetch");
-
+// Remember to install the library/module node-fetch as a dependecy
 
 
 app.get("/proxy", (req, res) => {
     fetch("https://www.google.dk")
-    .then(res => res.textConverted())
+    .then(res => res.textConverted()) 
     .then(body => res.send(body));
 });
+// if textConverted() is used also remember to install -> "npm install encoding"
+// removes minor flaws in the text when using forign symbols from the danish language
 
 const server = app.listen(process.env.PORT || 8080, (error) => {
     if (error) {
